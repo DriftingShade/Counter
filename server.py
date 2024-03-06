79ea07b785c9cb057153cac7df22272ff0129467
DriftingShade <sarus333@yahoo.com>
2024-03-05 22:59:11 -0700

    reformatting

--- server.py
+++ server.py
@@ -1,32 +1,38 @@
 from flask import Flask, render_template, request, redirect, session
+
 app = Flask(__name__)
-app.secret_key = 'you can run but you cant hide'
+app.secret_key = "you can run but you cant hide"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    if 'count' not in session:
-        session['count']=0
-    session['count']+=1
-    return render_template("index.html", count= session['count'])
+    if "count" not in session:
+        session["count"] = 0
+    session["count"] += 1
+    return render_template("index.html", count=session["count"])
 
-@app.route('/addtwo')
+
+@app.route("/addtwo")
 def addtwo():
-    session['count'] += 1
-    return redirect('/')
+    session["count"] += 1
+    return redirect("/")
 
-@app.route('/reset')
+
+@app.route("/reset")
 def reset():
-    session['count'] = 0
-    return redirect('/')
+    session["count"] = 0
+    return redirect("/")
 
-@app.route('/addcustom')
+
+@app.route("/addcustom")
 def addcustom():
-    session['custom'] = 0
-    session['count'] += session['custom']
-    return redirect('/')
+    if "custom" not in session:
+        session["custom"] = 0
+    session["count"] += session["custom"]
+    return redirect("/")
+
+
 # I ALMOST got this working, I just can't quite figure it out!!!  I will play with it some more.
-
-
 
 
 if __name__ == "__main__":
